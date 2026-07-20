09ec38d6b626a94e19cb241e2c5f4105db0b6db8
jason2000-cpu <jacksonmuturi2000@gmail.com>
2026-07-20 17:00:16 +0300

    update: Version updates 	- Did version updates for prisma and prisma clients 	- removed the deprecated use of prisma.$use and replaced with $extends 	- Changed the database host from neon to docker 	- added Seaweedfs file storage to store profile pictures and any other media that will need storage

--- src/prisma/prismaClient.ts
+++ src/prisma/prismaClient.ts
@@ -42,39 +42,3 @@ const prisma = new PrismaClient({ adapter }).$extends({
 })
 
 export default prisma;
-
-// prisma.$use(async (params, next) => {
-//     const result = await next(params);
-
-//     if(
-//         (params.model === 'Income' || params.model === 'Expenses') && 
-//         ['create', 'update', 'delete'].includes(params.action)
-//     ) {
-//         await synchronizeFinancialRecords(params)
-//     } else if( 
-//         params.model === 'Orders' && 
-//         ['create', 'update', 'delete'].includes(params.action)
-//     ){
-//         const order = result as { clientId: number }
-
-//         if(order && order.clientId) {
-//             const inProgressOrders = await prisma.orders.count({
-//                 where: {
-//                     clientId: order.clientId,
-//                     completionStatus: CompletionStatus.InProgress
-//                 },
-//             });
-
-//             await prisma.clients.update({
-//                 where: { id: order.clientId},
-//                 data: { activeOrder: inProgressOrders > 0 }
-//             })
-//         }
-//     }
-
-//     // console.log("RESULT::::", result);
-
-//     return result;
-// })
-
-// export default prisma
